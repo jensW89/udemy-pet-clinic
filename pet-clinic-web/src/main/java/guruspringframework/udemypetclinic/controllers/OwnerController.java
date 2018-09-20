24c95c61af56aef2d87177c7fef2b11b7c335b8c
Jens Wambach <jens.wambach@t-online.de>
2018-09-20 17:06:56 +0200

    added owners list and updated controller. closes #16

---
+++ pet-clinic-web/src/main/java/guruspringframework/udemypetclinic/controllers/OwnerController.java
@@ -0,0 +1,28 @@
+package guruspringframework.udemypetclinic.controllers;
+
+import guruspringframework.udemypetclinic.services.OwnerService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jensW89
+ */
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(Model model){
+
+
+        model.addAttribute("ownsers", ownerService.findAll());
+        return "owners/index";
+    }
+}
