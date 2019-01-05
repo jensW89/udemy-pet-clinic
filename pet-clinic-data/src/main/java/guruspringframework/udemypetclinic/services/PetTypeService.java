480d8d5d8b99ba8b4e8d4fb0cf7f66d6b3b5cc77
Jens Wambach <jens.wambach@t-online.de>
2019-01-05 19:50:49 +0100

    added pet type service. closes #29

---
+++ pet-clinic-data/src/main/java/guruspringframework/udemypetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package guruspringframework.udemypetclinic.services;
+
+import guruspringframework.udemypetclinic.model.PetType;
+
+/**
+ * Created by jensW89
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
