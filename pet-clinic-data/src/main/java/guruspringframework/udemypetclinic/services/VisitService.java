1580a7b87fcd26423bbc70d599fb82e7a3c433be
Jens Wambach <jens.wambach@t-online.de>
2019-01-09 17:54:09 +0100

    implement map impl visit service. closes #46

---
+++ pet-clinic-data/src/main/java/guruspringframework/udemypetclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package guruspringframework.udemypetclinic.services;
+
+import guruspringframework.udemypetclinic.model.Visit;
+
+/**
+ * Created by jensW89
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/guruspringframework/udemypetclinic/services/map/VistiMapService.java
@@ -0,0 +1,7 @@
+package guruspringframework.udemypetclinic.services.map;
+
+/**
+ * Created by jensW89
+ */
+public class VistiMapService {
+}
