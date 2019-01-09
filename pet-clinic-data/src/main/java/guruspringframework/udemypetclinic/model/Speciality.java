0b948c827b24fe1e39abda3b96edc27cf7cf0e64
Jens Wambach <jens.wambach@t-online.de>
2019-01-09 14:02:42 +0100

    updated vet to use JPA. closes #37

--- pet-clinic-data/src/main/java/guruspringframework/udemypetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guruspringframework/udemypetclinic/model/Speciality.java
@@ -1,10 +1,17 @@
 package guruspringframework.udemypetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by jensW89
  */
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
